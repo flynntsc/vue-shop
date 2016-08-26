8b404e516e2a39e65cc93fbda0d8a005c5434f84
flynntsc <flynntsc@qq.com>
2016-08-26 16:11:47 +0800

    购物车ui以及数量增减插件

--- src/views/shopping-cart.vue
+++ src/views/shopping-cart.vue
@@ -1,118 +1,121 @@
 <template>
-    <div class="v-shop">
-        <!-- 搜索 -->
-        <div class="v-hd">
-            <div class="v-back" @click="goback"><i class="iconfont">&#xe602;</i></div>
-            <div class="v-search" v-link="{path:'shop-products?focus=1'}">
-                <div class="vux-search-box">
-                    <div class="weui_search_bar" id="search_bar">
-                        <form class="weui_search_outer">
-                            <div class="vux-search-mask"></div>
-                            <div class="weui_search_inner">
-                                <i class="weui_icon_search"></i>
-                                <input type="text" class="weui_search_input" id="search_input" autocomplete="off" required="" placeholder="店内搜索商品">
-                                <a href="javascript:" class="weui_icon_clear" id="search_clear"></a>
-                            </div>
-                            <label for="search_input" class="weui_search_text" id="search_text">
-                                <i class="weui_icon_search"></i>
-                                <span>店内搜索商品</span>
-                            </label>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="v-catbtn" v-link="{path:'shop-category'}">
-                <i class="iconfont">&#xe603;</i>
-                <div class="txt">分类</div>
-            </div>
-        </div>
+    <div class="v-cart">
+        <!-- 头部 -->
+        <x-header :left-options="{showBack: false}" class="v-hd">
+            购物车
+            <i class="iconfont v-back" slot="left">&#xe602;</i>
+        </x-header>
 
         <div class="v-wrap">
-            <!-- 店铺头部 -->
-            <div class="v-shophd" :style="{backgroundImage:'url('+indexData.backgroundImage+')'}">
-                <div class="logo"></div>
-                <div class="data">
-                    <div class="name">{{indexData.name}}</div>
-                    <div class="nums">销量{{indexData.sales}} 共{{indexData.total}}件商品</div>
-                    <div class="more" v-if="indexData.isSenior">
-                        <div class="ren">认</div> 企业实名认证
-                    </div>
+            <div class="v-cell">
+                <div class="v-cellhd">
+                    <label>
+                        <input type="checkbox" name="" id=""> 汇管车平台自营店
+                    </label>
                 </div>
-                <div class="mark"></div>
-            </div>
-
-            <!-- 轮播图 -->
-            <swiper :list="indexData.swiperList" :auto="true" class="v-swiper"></swiper>
-
-            <!-- 优惠劵 -->
-            <div class="v-coupons">
-                <img src="../assets/images/_coupons.jpg" alt="" class="img">
-            </div>
-
-            <!-- 商家推荐 -->
-            <div class="v-list">
-                <div class="title">商家推荐</div>
-                <div class="v-prolist">
-                    <div class="v-pro" v-for="items of indexData.hotList" v-link="{path:'product-detail',query:{shop:items.shop,pro:items.id}}">
-                        <div class="hd">
-                            <img :src="items.image" alt="" class="img">
-                        </div>
-                        <div class="bd">
-                            <div class="name">{{items.name}}</div>
-                            <div class="data">
-                                <span class="pri">￥{{items.price}}</span>
-                                <span class="sales">已售出：{{items.sales}}件</span>
-                            </div>
+                <div class="v-cellbd">
+                    <div class="sel">
+                        <label>
+                            <input type="checkbox" name="" id="">
+                        </label>
+                    </div>
+                    <div class="pro">
+                        <img src="http://temp.im/180x80" alt="">
+                    </div>
+                    <div class="box">
+                        <div class="name">米其林-XZE 高档绝无仅有卓多喝点开房大厦卡车载重轮胎全新正品</div>
+                        <div class="price">￥2499.00</div>
+                        <div class="nums">
+                            <number2></number2>
                         </div>
                     </div>
                 </div>
-            </div>
-            <!-- 新品上市 -->
-            <div class="v-list">
-                <div class="title">新品上市</div>
-                <div class="v-prolist">
-                    <div class="v-pro" v-for="items of indexData.newList" v-link="{path:'product-detail',query:{shop:items.shop,pro:items.id}}">
-                        <div class="hd">
-                            <img :src="items.image" alt="" class="img">
-                        </div>
-                        <div class="bd">
-                            <div class="name">{{items.name}}</div>
-                            <div class="data">
-                                <span class="pri">￥{{items.price}}</span>
-                                <span class="sales">已售出：{{items.sales}}件</span>
-                            </div>
+                <div class="v-cellbd">
+                    <div class="sel">
+                        <label>
+                            <input type="checkbox" name="" id="">
+                        </label>
+                    </div>
+                    <div class="pro">
+                        <img src="http://temp.im/80x80" alt="">
+                    </div>
+                    <div class="box">
+                        <div class="name">米其林-XZE 高档绝无仅有卓多喝点开房大厦卡车载重轮胎全新正品</div>
+                        <div class="price">￥2499.00</div>
+                        <div class="nums">
+                            <number2></number2>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- 所有商品 -->
-            <div class="v-goall" v-link="{path:`shop-products?shop=${shop}`}">查看所有商品</div>
+            <div class="v-cell">
+                <div class="v-cellhd">
+                    <label>
+                        <input type="checkbox" name="" id=""> 汇管车平台自营店2222
+                    </label>
+                </div>
+                <div class="v-cellbd">
+                    <div class="sel">
+                        <label>
+                            <input type="checkbox" name="" id="">
+                        </label>
+                    </div>
+                    <div class="pro">
+                        <img src="http://temp.im/80x80" alt="">
+                    </div>
+                    <div class="box">
+                        <div class="name">米其林-XZE 高档绝无仅有卓多喝点开房大厦卡车载重轮胎全新正品</div>
+                        <div class="price">￥2499.00</div>
+                        <div class="nums">
+                            <number2></number2>
+                        </div>
+                    </div>
+                </div>
+                <div class="v-cellbd">
+                    <div class="sel">
+                        <label>
+                            <input type="checkbox" name="" id="">
+                        </label>
+                    </div>
+                    <div class="pro">
+                        <img src="http://temp.im/80x80" alt="">
+                    </div>
+                    <div class="box">
+                        <div class="name">米其林-XZE 高档绝无仅有卓多喝点开房大厦卡车载重轮胎全新正品</div>
+                        <div class="price">￥2499.00</div>
+                        <div class="nums">
+                            <number2></number2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <!-- 底部菜单 -->
-        <div class="v-fnav">
-            <div class="it">店铺介绍</div>
-            <div class="it">店铺公告</div>
+        <div class="v-fd">
+            <div class="sel">
+                <label>
+                    <input type="checkbox" name="" id=""> 全选
+                </label>
+            </div>
+            <div class="total">合计：￥<span class="pri">0.00</span></div>
+            <div class="buy">立即购买</div>
         </div>
     </div>
 </template>
 
 <script>
 import {
-    Swiper,
-    Tab,
-    TabItem,
-} from 'vux/src/components';
-import SearchCat from '../components/search-cat/index.vue'
-import ProductsList from '../components/products-list/index.vue'
+    XHeader,
+} from 'vux/src/components'
+
+import Number2 from '../components/x-number2/index.vue'
 
 
 export default {
     components: {
-        Swiper,
-        Tab,
-        TabItem,
-        ProductsList,
+        XHeader,
+        Number2,
     },
     data() {
         return {
@@ -154,203 +157,139 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import '../assets/styles/iconfont.scss';
-.v-shop {
+.v-cart {
     background: #efefef;
     padding: 44px 0;
+}
+
+.v-hd {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 5;
+    width: 100%;
+    height: 44px;
+    background: #c50a1d;
+}
+
+.v-back {
+    font-size: 22px;
+    color: #fff;
 }
 
 .v-wrap {
     // flex: 1;
 }
 
-// 搜索头部
-.v-hd {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 44px;
-    display: flex;
-    z-index: 5;
-    align-items: center;
-    background-color: #c50a1d;
-}
-
-.v-search {
-    flex: 1;
-    padding: 8px;
-    .weui_search_bar {
-        padding: 0;
-    }
-}
-
-.v-back {
-    width: 44px;
-    line-height: 44px;
-    text-align: center;
-    color: #fff;
-    i {
-        font-size: 22px;
-    }
-}
-
-.v-catbtn {
-    padding-right: 10px;
-    line-height: 18px;
-    text-align: center;
-    color: #fff;
-    i {
-        height: 14px;
-        font-size: 24px;
-    }
-    .txt {
-        height: 14px;
-        font-size: 12px;
-    }
-}
-
-// 店铺头部
-.v-shophd {
-    position: relative;
-    width: 100%;
-    padding-bottom: 33%;
-    background: url('') no-repeat 50% 50% / 100% auto transparent;
-    .logo {
-        position: absolute;
-        bottom: 0;
-        left: 15px;
-        width: 80px;
-        height: 80px;
-        background: url('../assets/images/_logo.jpg') no-repeat 0 0 / 100% auto transparent;
-    }
-    .data {
-        position: absolute;
-        left: 105px;
-        bottom: 5px;
-        .name {
-            font-size: 16px;
-            color: #333;
-        }
-        .nums {
-            line-height: 30px;
-            font-size: 14px;
-            color: #999;
-        }
-        .more {
-            line-height: 16px;
-            font-size: 14px;
-            color: #f00;
-            .ren {
-                display: inline-block;
-                border-radius: 2px;
-                pading: 2px;
-                color: #fff;
-                background: #cf7207;
-            }
-        }
-    }
-    .mark {
-        position: absolute;
-        right: 15px;
-        bottom: 0;
-        width: 50px;
-        height: 50px;
-        background: url('../assets/images/_mark.jpg') no-repeat 0 0 / 100% auto transparent;
-    }
-}
-
-// 轮播图
-.v-swiper {
-    margin-top: 10px;
-}
-
-// 优惠券
-.v-coupons {
-    .img {
-        width: 100%;
-    }
-}
-
-// 产品列表
-.v-list {
-    margin-bottom: 10px;
-    padding: 10px 5px;
-    background: #fff;
-    .title {
-        padding: 0 5px;
-    }
-}
-
-.v-prolist {
-    overflow: hidden;
-    .v-pro {
-        box-sizing: border-box;
-        width: 50%;
-        float: left;
-        padding: 0 5px;
-        .hd {
-            height: 145px;
-            overflow: hidden;
-            .img {
-                width: 100%;
-                height: 100%;
-            }
-        }
-        .bd {
-            .name {
-                display: -webkit-box;
-                height: 24px;
-                line-height: 24px;
-                overflow: hidden;
-                text-overflow: ellipsis;
-                -webkit-box-orient: vertical;
-                -webkit-line-clamp: 1;
-                font-size: 16px;
-            }
-            .data {
-                font-size: 14px;
-                .pri {
-                    color: #f00;
-                }
-                .sales {
-                    font-size: 12px;
-                    color: #888;
-                }
-            }
-        }
-    }
-}
-
-// 查看所有
-.v-goall {
-    padding: 5px 0;
-    border-bottom: 10px solid #efefef;
-    font-size: 14px;
-    text-align: center;
-    background: #fff;
-}
-
 // 底部菜单
-.v-fnav {
+.v-fd {
     position: fixed;
     bottom: 0;
     left: 0;
     width: 100%;
     z-index: 5;
     display: flex;
+    height: 44px;
+    line-height: 44px;
     text-align: center;
-    color: #fff;
-    .it {
-        flex: 1;
-        height: 44px;
-        line-height: 44px;
-        &:nth-child(1) {
-            background: rgba(8, 8, 8, .8);
-            color: #fc9;
+    background: #fff;
+    color: #666;
+    &:before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        border-top: 1px solid #ececec;
+        transform: scaleY(.5);
+    }
+    .sel {
+        padding: 0 15px;
+        font-size: 14px;
+    }
+    .total {
+        flex: 2;
+        font-size: 14px;
+        text-align: right;
+        padding-right: 10px;
+        .pri {
+            font-size: 16px;
+            color: #f00;
         }
-        &:nth-child(2) {
-            background: rgba(255, 51, 0, .8);
-            color: #fff;
+    }
+    .buy {
+        flex: 1;
+        padding: 0 15px;
+        background: rgba(255, 51, 0, .8);
+        color: #fff;
+    }
+}
+
+// 购物车
+.v-cell {
+    margin-bottom: 10px;
+    background: #fff;
+}
+
+.v-cellhd {
+    padding: 10px 15px;
+    background: #f8f8f8;
+}
+
+.v-cellbd {
+    position: relative;
+    display: flex;
+    padding: 5px 15px 5px 0;
+    &:after {
+        content: '';
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        border-bottom: 1px solid #ececec;
+        transform: scaleY(.5);
+    }
+    .sel {
+        line-height: 1;
+        label {
+            display: block;
+            padding: 30px 10px 30px 15px;
+        }
+    }
+    .pro {
+        // margin-right: 10px;
+        width: 80px;
+        height: 80px;
+        border: 1px solid #ececec;
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+    .box {
+        flex: 1;
+        position: relative;
+        margin-left: 10px;
+        .name {
+            display: -webkit-box;
+            height: 40px;
+            line-height: 20px;
+            font-size: 14px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;
+        }
+        .price {
+            position: absolute;
+            bottom: 5px;
+            left: 0;
+            color: #f00;
+        }
+        .nums {
+            position: absolute;
+            right: 0;
+            bottom: 5px;
         }
     }
 }
