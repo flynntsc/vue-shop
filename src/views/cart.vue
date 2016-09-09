755b899e8a536a437f51c6793f74fb46da64f548
flynntsc <flynntsc@qq.com>
2016-09-09 17:37:13 +0800

    购物车数据过滤

--- src/views/cart.vue
+++ src/views/cart.vue
@@ -7,16 +7,16 @@
         </x-header>
 
         <div class="v-wrap">
-            <div class="v-cell" v-for="items of cartPros">
-                <div class="v-cellhd">
+            <div class="v-cell" v-for="items of cartPros" :class="{'v-cell-1' : $index === 0 || items.hz_goods_mange.supplier_id !== cartPros[$index - 1].hz_goods_mange.supplier_id}">
+                <div class="v-cellhd" v-if="$index === 0 || items.hz_goods_mange.supplier_id !== cartPros[$index - 1].hz_goods_mange.supplier_id">
                     <label>
-                        <input type="radio" name="shop" id=""> {{items.hz_goods_mange.supplier_name}}
+                        <input type="radio" name="shop" id="" @click="radioFn($index)" value="selected" v-model="items.selected"> {{items.hz_goods_mange.supplier_name}}
                     </label>
                 </div>
                 <div class="v-cellbd">
                     <div class="sel">
                         <label>
-                            <input type="checkbox" name="" id="">
+                            <input type="checkbox" name="" id="" v-model="items.checked" @click="checkboxFn($index)">
                         </label>
                     </div>
                     <div class="pro">
@@ -26,63 +26,23 @@
                         <div class="name" v-link="{path:'product-detail',query:{pro:items.goods_id}}">{{items.hz_goods_mange.goods_name}}</div>
                         <div class="price">￥{{items.hz_goods_mange.price_new || items.hz_goods_mange.current_price}}</div>
                         <div class="nums">
-                            <number2 :value.sync="items.amount"></number2>
+                            <number2 :value.sync="items.amount" @on-change="numChange"></number2>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- <div class="v-cell">
-                <div class="v-cellhd">
-                    <label>
-                        <input type="radio" name="shop" id=""> 汇管车平台自营店2222
-                    </label>
-                </div>
-                <div class="v-cellbd">
-                    <div class="sel">
-                        <label>
-                            <input type="checkbox" name="" id="">
-                        </label>
-                    </div>
-                    <div class="pro">
-                        <img src="http://temp.im/80x80" alt="">
-                    </div>
-                    <div class="box">
-                        <div class="name">米其林-XZE 高档绝无仅有卓多喝点开房大厦卡车载重轮胎全新正品</div>
-                        <div class="price">￥2499.00</div>
-                        <div class="nums">
-                            <number2></number2>
-                        </div>
-                    </div>
-                </div>
-                <div class="v-cellbd">
-                    <div class="sel">
-                        <label>
-                            <input type="checkbox" name="" id="">
-                        </label>
-                    </div>
-                    <div class="pro">
-                        <img src="http://temp.im/80x80" alt="">
-                    </div>
-                    <div class="box">
-                        <div class="name">米其林-XZE 高档绝无仅有卓多喝点开房大厦卡车载重轮胎全新正品</div>
-                        <div class="price">￥2499.00</div>
-                        <div class="nums">
-                            <number2></number2>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
         </div>
 
         <!-- 底部菜单 -->
         <div class="v-ft">
-            <!-- <div class="sel">
+            <div class="sel">
                 <label>
                     <input type="checkbox" name="" id=""> 全选
                 </label>
-            </div> -->
-            <div class="total">合计：￥<span class="pri">0.00</span></div>
-            <div class="buy">立即购买</div>
+            </div>
+            <div class="total">合计：￥<span class="pri">{{cartSum}}</span></div>
+            <div class="buy" v-show="cartSum > 0" @click="goBuy">立即购买</div>
+            <div class="empty" v-else>请先选择</div>
         </div>
     </div>
 </template>
@@ -93,10 +53,6 @@ import {
 } from 'vux/src/components'
 
 import Number2 from '../components/x-number2/index.vue'
-
-function formatArr(arr) {
-    // 格式化为二维数组？？？
-}
 
 export default {
     components: {
@@ -113,10 +69,10 @@ export default {
                     'picture_url': 'http://temp.im/100x100',
                     'amount': 1,
                     'hz_goods_mange': {
-                        'supplier_id': 14201,
+                        'supplier_id': 1,
                         'supplier_name': '车队汇',
                         'goods_name': '轮胎0933',
-                        'current_price': '999',
+                        'current_price': '9929',
                         'price_new': '999',
                     }
                 }, {
@@ -125,31 +81,78 @@ export default {
                     'picture_url': 'http://temp.im/100x100',
                     'amount': 2,
                     'hz_goods_mange': {
-                        'supplier_id': 19095,
+                        'supplier_id': 2,
                         'supplier_name': '车队汇222',
                         'goods_name': '壳牌机油1000',
-                        'current_price': '999',
+                        'current_price': '9190',
                         'price_new': '999',
+                    }
+                }, {
+                    'id': '1222',
+                    'goods_id': 138,
+                    'picture_url': 'http://temp.im/100x100',
+                    'amount': 1,
+                    'hz_goods_mange': {
+                        'supplier_id': 1,
+                        'supplier_name': '车队汇',
+                        'goods_name': '轮胎0933',
+                        'current_price': '423',
+                        'price_new': '391',
                     }
                 }],
             },
             // 获取后转换处理
             cartPros: [],
+            // 选中的店铺id
+            shopSeled: '',
+            // 总价
+            cartSum: 0.00,
         }
     },
-    created() {
+    ready() {
         let url = `/api/shopping/shop_cart/show_shop_cart.htm`
         this.$http.get(url).then(res => {
             let data = JSON.parse(res.data)
+
+            // 遍历添加的属性
+            let objSel = {
+                selected: '',
+                checked: false
+            }
             this.cartData = Object.assign({}, this.cartData, data)
-            this.cartPros = this.cartData.cartList
+            this.cartPros = this.cartData.cartList.sort((a, b) => a.hz_goods_mange.supplier_id - b.hz_goods_mange.supplier_id).map(v => Object.assign(v, objSel))
         })
     },
-    ready() {},
+    watch: {
+        // 监听数据并更改总价
+        cartPros(arr, oldarr) {
+            this.cartSum = arr.filter(v => v.checked === true).reduce((a, b) => a + b.amount * (b.hz_goods_mange.price_new || b.hz_goods_mange.current_price), 0)
+        },
+    },
     methods: {
         // 返回
         goback() {
             history.back()
+        },
+        // 点击店铺单选按钮
+        radioFn(i) {
+            this.shopSeled = this.cartPros[i].hz_goods_mange.supplier_id
+            console.dir(this.cartPros)
+            this.cartPros = this.cartPros.map(v => {
+                v.checked = v.hz_goods_mange.supplier_id === this.shopSeled
+                return v
+            })
+        },
+        // 如何更改数据来改变状态？？？？
+        checkboxFn(i) {
+
+        },
+        // 更改数量-value非双向绑定嘛？？？？
+        numChange(i) {
+
+        },
+        goBuy() {
+
         },
     }
 }
@@ -222,12 +225,20 @@ export default {
         background: rgba(255, 51, 0, .8);
         color: #fff;
     }
+    .empty {
+        flex: 1;
+        padding: 0 15px;
+        background: rgba(225, 225, 225, .8);
+        color: #fff;
+    }
 }
 
 // 购物车
 .v-cell {
-    margin-bottom: 10px;
     background: #fff;
+    &.v-cell-1 {
+        margin-top: 10px;
+    }
 }
 
 .v-cellhd {
